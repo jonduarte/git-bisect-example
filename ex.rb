712db34813ba1bcfa97c22581275a821a1cd5c6f
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2017-03-05 23:02:36 +0000

    Initial commit

---
+++ ex.rb
@@ -0,0 +1,19 @@
+class Print
+  def me
+    'me'
+  end
+end
+
+if $0 == $PROGRAM_NAME
+  require 'minitest/autorun'
+
+  class TestMeme < MiniTest::Unit::TestCase
+    def setup
+      @print = Print.new
+    end
+
+    def test_returns_me
+      assert_equal 'me', @print.me
+    end
+  end
+end
